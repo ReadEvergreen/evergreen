1adaaef7618d52a5dd3d926a9b50eb14441e61ec
Aaron Wolfson <awolfson0@gmail.com>
2018-01-27 14:34:41 -0600

    Requires Resource to have a URL.

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -16,8 +16,24 @@
 
 RSpec.describe Resource, type: :model do
 
+  subject { Resource.new(title: title, url: url) }
+
+  let(:title) { "Test Resource" }
+  let(:url) { "testurl.example.com" }
+
+  it "is valid" do
+    expect(subject).to be_valid
+  end
+
   it { should belong_to(:owner) }
   it { should have_many(:upvotes).dependent(:destroy) }
   it { should have_many(:upvoted_users).through(:upvotes) }
 
+  describe "#url" do
+    it "must be present" do
+      subject.url = nil
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
