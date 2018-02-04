fe4662bf26042af0242a1abd2c65bba847f93bbc
Aaron Wolfson <awolfson0@gmail.com>
2018-02-04 12:49:35 -0600

    Updates annotations.

--- app/models/collection_resource.rb
+++ app/models/collection_resource.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: collection_resources
+#
+#  collection_id :integer          not null
+#  resource_id   :integer          not null
+#
+
 class CollectionResource < ActiveRecord::Base
   belongs_to :collection
   belongs_to :resource
--- spec/factories/collection_resources.rb
+++ spec/factories/collection_resources.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: collection_resources
+#
+#  collection_id :integer          not null
+#  resource_id   :integer          not null
+#
+
 FactoryBot.define do
   factory :collection_resource do
     association :collection
